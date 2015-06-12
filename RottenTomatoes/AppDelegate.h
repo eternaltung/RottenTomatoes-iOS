1bf9623e06b2c8f6bd9b93ff689bf68937e13bc9
eternaltung <eternaltung@gmail.com>
2015-06-12 14:59:52 +0800

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Shih-Ming Tung on 6/12/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Shih-Ming Tung on 6/12/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
